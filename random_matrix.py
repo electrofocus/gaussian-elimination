e42a322130bb74e0c90fb44d099b7d6917911e74
Amir Mullagaliev <amirmullagaliev@gmail.com>
2020-12-22 11:00:13 +0500

    Create random input generator

---
+++ random_matrix.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+n = int(input("Type matrix size: "))
+
+A = np.random.rand(n, n)
+X = np.random.rand(n)
+
+b = A @ X
+
+with open("input.txt", "w") as file_1:
+    np.savetxt(file_1, [n], fmt="%d")
+    np.savetxt(file_1, A, fmt="%.2f")
+    np.savetxt(file_1, b, fmt="%.2f")
+
+with open("X.txt", "w") as file_2:
+    np.savetxt(file_2, X, fmt="%.2f")
+
+print("You can find your data in 'input.txt' and 'X.txt' files")
