933d9a578440ca549ea83f5101765f0756249931
Amir Mullagaliev <amirmullagaliev@gmail.com>
2020-12-29 19:30:02 +0500

    Небольшие правки в заголовочном файле

--- lss_71_01.h
+++ lss_71_01.h
@@ -2,8 +2,7 @@
 // Created by Amir Mullagaliev on 12/16/2020.
 //
 
-#ifndef PRACTICUM_7_2_LSS_71_01_H
-#define PRACTICUM_7_2_LSS_71_01_H
+#pragma once
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,5 +11,3 @@
 int lss_71_01(int n, double *A, double *B, double *X, double *tmp);
 void printSystem(int n, double *A, double *B);
 size_t lss_memsize_71_01(int n);
-
-#endif  // PRACTICUM_7_2_LSS_71_01_H
