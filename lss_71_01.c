77ac32e5b10cc77ac9987d78f7adaf0f7a272b70
Amir Mullagaliev <amirmullagaliev@gmail.com>
2021-01-13 14:39:19 +0500

    Indentation refactoring

--- lss_71_01.c
+++ lss_71_01.c
@@ -12,161 +12,161 @@ extern int flag_debug;
 size_t lss_memsize_71_01(int n) { return n * sizeof(double); }
 
 int maxColIdx(int n, int k, double *A) {
-  int j = 0, colIdx = -1;
-  double max = 0;
+    int j = 0, colIdx = -1;
+    double max = 0;
 
-  for (j = k; j < n; j++) {
-    if (fabs(A[k * n + j]) - fabs(max) > EPS) {
-      max = fabs(A[k * n + j]);
-      colIdx = j;
+    for (j = k; j < n; j++) {
+        if (fabs(A[k * n + j]) - fabs(max) > EPS) {
+            max = fabs(A[k * n + j]);
+            colIdx = j;
+        }
     }
-  }
 
-  return colIdx;
+    return colIdx;
 }
 
 void swapCols(int n, int k, int idx, double *A, int *cols) {
-  int i = 0;
-  double tmp;
+    int i = 0;
+    double tmp;
 
-  // Перестановка колонок
+    // Перестановка колонок
 
-  for (i = 0; i < n; i++) {
-    tmp = A[i * n + k];
-    A[i * n + k] = A[i * n + idx];
-    A[i * n + idx] = tmp;
-  }
+    for (i = 0; i < n; i++) {
+        tmp = A[i * n + k];
+        A[i * n + k] = A[i * n + idx];
+        A[i * n + idx] = tmp;
+    }
 
-  // Перестановка номеров колонок
+    // Перестановка номеров колонок
 
-  tmp = cols[k];
-  cols[k] = cols[idx];
-  cols[idx] = tmp;
+    tmp = cols[k];
+    cols[k] = cols[idx];
+    cols[idx] = tmp;
 }
 
 void printCols(int n, int *cols) {
-  int k;
+    int k;
 
-  printf("\ncols: ");
-  for (k = 0; k < n; k++) {
-    printf("%d ", cols[k]);
-  }
+    printf("\ncols: ");
+    for (k = 0; k < n; k++) {
+        printf("%d ", cols[k]);
+    }
 }
 
 void printX(int n, double *X) {
-  int k;
+    int k;
 
-  printf("\n   X: ");
-  for (k = 0; k < n; k++) {
-    printf("%lf ", X[k]);
-  }
+    printf("\n   X: ");
+    for (k = 0; k < n; k++) {
+        printf("%lf ", X[k]);
+    }
 }
 
 int lss_71_01(int n, double *A, double *B, double *X, double *tmp) {
-  int i = 0, k = 0, j = 0, idx = 0;
-  int *cols = (int *)tmp;
-  double t, sum = 0, divider = 1, multiplier = 0;
+    int i = 0, k = 0, j = 0, idx = 0;
+    int *cols = (int *)tmp;
+    double t, sum = 0, divider = 1, multiplier = 0;
 
-  // Запомнить позиции колонок и проинициализировать вектор X
+    // Запомнить позиции колонок и проинициализировать вектор X
 
-  for (int i = 0; i < n; i++) {
-    cols[i] = i;
-    X[i] = -1;
-  }
+    for (int i = 0; i < n; i++) {
+        cols[i] = i;
+        X[i] = -1;
+    }
 
-  // Прямой ход
+    // Прямой ход
 
-  for (k = 0; k < n; k++) {
-    idx = maxColIdx(n, k, A);
+    for (k = 0; k < n; k++) {
+        idx = maxColIdx(n, k, A);
 
-    // Если в строке все нули
+        // Если в строке все нули
 
-    if (idx == -1) {
-      // Если в правой части не ноль
+        if (idx == -1) {
+            // Если в правой части не ноль
 
-      if (fabs(B[k]) > EPS) {
-        if (flag_debug) {
-          printf("\nSystem has no solution\n");
+            if (fabs(B[k]) > EPS) {
+                if (flag_debug) {
+                    printf("\nSystem has no solution\n");
+                }
+                return 1;
+            }
+            X[k] = 0;
+            continue;
         }
-        return 1;
-      }
-      X[k] = 0;
-      continue;
+
+        // Выход на последней итерации
+
+        if (k == n - 1) {
+            break;
+        }
+
+        if (idx != k) {
+            swapCols(n, k, idx, A, cols);
+
+            if (flag_debug) {
+                printf("%d <-> %d\n\n", k, idx);
+            }
+        }
+
+        if (flag_debug) {
+            printSystem(n, A, B);
+        }
+
+        for (i = k + 1; i < n; i++) {
+            multiplier = A[i * n + k] / A[k * n + k];
+
+            for (j = 0; j < n; j++) {
+                A[i * n + j] = A[i * n + j] - A[k * n + j] * multiplier;
+            }
+            B[i] = B[i] - B[k] * multiplier;
+        }
+
+        if (flag_debug) {
+            printSystem(n, A, B);
+        }
     }
 
-    // Выход на последней итерации
+    // Обратный ход
 
-    if (k == n - 1) {
-      break;
+    for (k = n - 1; k > -1; k--) {
+        if (X[k] != 0) {
+            sum = 0;
+            for (j = k + 1; j < n; j++) {
+                sum += A[k * n + j] * X[j];
+            }
+            X[k] = (B[k] - sum) / A[k * n + k];
+        }
     }
 
-    if (idx != k) {
-      swapCols(n, k, idx, A, cols);
-
-      if (flag_debug) {
-        printf("%d <-> %d\n\n", k, idx);
-      }
-    }
+    // Перестановка элементов вектора-ответа
 
     if (flag_debug) {
-      printSystem(n, A, B);
+        printCols(n, cols);
+        printX(n, X);
+        printf("\n\n");
     }
 
-    for (i = k + 1; i < n; i++) {
-      multiplier = A[i * n + k] / A[k * n + k];
+    for (i = 0; i < n; i++) {
+        for (j = i; j < n; j++) {
+            if ((int)cols[j] == i) {
+                t = X[i];
+                X[i] = X[j];
+                X[j] = t;
 
-      for (j = 0; j < n; j++) {
-        A[i * n + j] = A[i * n + j] - A[k * n + j] * multiplier;
-      }
-      B[i] = B[i] - B[k] * multiplier;
+                t = cols[i];
+                cols[i] = cols[j];
+                cols[j] = t;
+
+                break;
+            }
+        }
+
+        if (flag_debug) {
+            printCols(n, cols);
+            printX(n, X);
+            printf("\n\n");
+        }
     }
 
-    if (flag_debug) {
-      printSystem(n, A, B);
-    }
-  }
-
-  // Обратный ход
-
-  for (k = n - 1; k > -1; k--) {
-    if (X[k] != 0) {
-      sum = 0;
-      for (j = k + 1; j < n; j++) {
-        sum += A[k * n + j] * X[j];
-      }
-      X[k] = (B[k] - sum) / A[k * n + k];
-    }
-  }
-
-  // Перестановка элементов вектора-ответа
-
-  if (flag_debug) {
-    printCols(n, cols);
-    printX(n, X);
-    printf("\n\n");
-  }
-
-  for (i = 0; i < n; i++) {
-    for (j = i; j < n; j++) {
-      if ((int)cols[j] == i) {
-        t = X[i];
-        X[i] = X[j];
-        X[j] = t;
-
-        t = cols[i];
-        cols[i] = cols[j];
-        cols[j] = t;
-
-        break;
-      }
-    }
-
-    if (flag_debug) {
-      printCols(n, cols);
-      printX(n, X);
-      printf("\n\n");
-    }
-  }
-
-  return 0;
+    return 0;
 }
--- main_lss_71_01.c
+++ main_lss_71_01.c
@@ -9,279 +9,279 @@
 char flag_debug = 0, flag_errors = 0, flag_print_matrix = 0, flag_time = 0, flag_help = 0, max_iter = 0;
 
 unsigned int stringLength(char const *input) {
-  int length = 0;
+    int length = 0;
 
-  while (input[length] != '\0') {
-    length++;
-  }
+    while (input[length] != '\0') {
+        length++;
+    }
 
-  return length;
+    return length;
 }
 
 int validateFile(char const *filename) {
-  FILE *f = fopen(filename, "rb");
-  int result = f != NULL;
+    FILE *f = fopen(filename, "rb");
+    int result = f != NULL;
 
-  fclose(f);
-  return result;
+    fclose(f);
+    return result;
 }
 
 int validateParams(int argc, char **argv) {
-  int i, count = 0;
+    int i, count = 0;
 
-  if (argc == 1) {
-    return 0;
-  }
-
-  for (i = 1; i < argc; i++) {
-    if (argv[i][0] == '-' && stringLength(argv[i]) == 2) {
-      switch (argv[i][1]) {
-        case 'd': {
-          flag_debug = 1;
-          break;
-        }
-        case 'h': {
-          flag_help = 1;
-          break;
-        }
-        case '?': {
-          flag_help = 1;
-          break;
-        }
-        case 'e': {
-          flag_errors = 1;
-          break;
-        }
-        case 'p': {
-          flag_print_matrix = 1;
-          break;
-        }
-        case 't': {
-          flag_time = 1;
-          break;
-        }
-        default: {
-          return 1;  // no such flag
-        }
-      }
-    } else if (argv[i][0] == '-') {
-      return 1;  // no such flag
-    } else {
-      count++;
+    if (argc == 1) {
+        return 0;
     }
-  }
 
-  if (count > 2) {
-    return 2;  // too much filenames
-  }
+    for (i = 1; i < argc; i++) {
+        if (argv[i][0] == '-' && stringLength(argv[i]) == 2) {
+            switch (argv[i][1]) {
+                case 'd': {
+                    flag_debug = 1;
+                    break;
+                }
+                case 'h': {
+                    flag_help = 1;
+                    break;
+                }
+                case '?': {
+                    flag_help = 1;
+                    break;
+                }
+                case 'e': {
+                    flag_errors = 1;
+                    break;
+                }
+                case 'p': {
+                    flag_print_matrix = 1;
+                    break;
+                }
+                case 't': {
+                    flag_time = 1;
+                    break;
+                }
+                default: {
+                    return 1;  // no such flag
+                }
+            }
+        } else if (argv[i][0] == '-') {
+            return 1;  // no such flag
+        } else {
+            count++;
+        }
+    }
 
-  return 0;
+    if (count > 2) {
+        return 2;  // too much filenames
+    }
+
+    return 0;
 }
 
 int readInput(char const *inputFile, double **A, double **B, int *n) {
-  int i, checkInput;
-  FILE *input = fopen(inputFile, "r");
+    int i, checkInput;
+    FILE *input = fopen(inputFile, "r");
 
-  checkInput = fscanf(input, "%d", n);
-  if (checkInput == EOF) {
-    return 1;  // unexpected end of file
-  }
-
-  if (checkInput == 0 || *n <= 0) {
-    return 2;  // (n in not %d) or (n <= 0)
-  }
-
-  *A = malloc((*n) * (*n) * sizeof(double));
-  *B = malloc((*n) * sizeof(double));
-
-  for (i = 0; i < (*n) * (*n); i++) {
-    checkInput = fscanf(input, "%lf", (*A + i));
+    checkInput = fscanf(input, "%d", n);
     if (checkInput == EOF) {
-      return 3;  // unexpected end of file
+        return 1;  // unexpected end of file
     }
-    if (checkInput == 0) {
-      return 4;  // value is not %lf
-    }
-  }
 
-  for (i = 0; i < (*n); i++) {
-    checkInput = fscanf(input, "%lf", (*B + i));
-    if (checkInput == EOF) {
-      return 3;  // unexpected end of file
+    if (checkInput == 0 || *n <= 0) {
+        return 2;  // (n in not %d) or (n <= 0)
     }
-    if (checkInput == 0) {
-      return 4;  // value is not %lf
-    }
-  }
 
-  fclose(input);
-  return 0;
+    *A = malloc((*n) * (*n) * sizeof(double));
+    *B = malloc((*n) * sizeof(double));
+
+    for (i = 0; i < (*n) * (*n); i++) {
+        checkInput = fscanf(input, "%lf", (*A + i));
+        if (checkInput == EOF) {
+            return 3;  // unexpected end of file
+        }
+        if (checkInput == 0) {
+            return 4;  // value is not %lf
+        }
+    }
+
+    for (i = 0; i < (*n); i++) {
+        checkInput = fscanf(input, "%lf", (*B + i));
+        if (checkInput == EOF) {
+            return 3;  // unexpected end of file
+        }
+        if (checkInput == 0) {
+            return 4;  // value is not %lf
+        }
+    }
+
+    fclose(input);
+    return 0;
 }
 
 void writeAnswer(char const *outputFile, int n, const double *X, int result) {
-  int i;
-  FILE *output = fopen(outputFile, "w");
+    int i;
+    FILE *output = fopen(outputFile, "w");
 
-  if (result != 0) {
-    fprintf(output, "%d\n", 0);
-  } else {
-    fprintf(output, "%d\n", n);
-    for (i = 0; i < n; i++) {
-      fprintf(output, "%1.9lf\n", *(X + i));
+    if (result != 0) {
+        fprintf(output, "%d\n", 0);
+    } else {
+        fprintf(output, "%d\n", n);
+        for (i = 0; i < n; i++) {
+            fprintf(output, "%1.9lf\n", *(X + i));
+        }
     }
-  }
 
-  fclose(output);
+    fclose(output);
 }
 
 void printHelp() {
-  printf(
-      "Usage: lss [input_file_name] [output_file_name] [options]\n"
-      "Where options include:\n"
-      " -d                print debug messages [default OFF]\n"
-      " -e                print errors [default OFF]\n"
-      " -p                print matrix [default OFF]\n"
-      " -t                print execution time [default OFF]\n"
-      " -h, -?            print this and exit\n");
+    printf(
+        "Usage: lss [input_file_name] [output_file_name] [options]\n"
+        "Where options include:\n"
+        " -d                print debug messages [default OFF]\n"
+        " -e                print errors [default OFF]\n"
+        " -p                print matrix [default OFF]\n"
+        " -t                print execution time [default OFF]\n"
+        " -h, -?            print this and exit\n");
 }
 
 void printSystem(int n, const double *A, const double *B) {
-  int i, j;
-  for (i = 0; i < n; i++) {
-    for (j = 0; j < n; j++) {
-      printf("%1.9lf ", A[i * n + j]);
+    int i, j;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            printf("%1.9lf ", A[i * n + j]);
+        }
+        printf("\t%1.9lf", B[i]);
+        printf("\n");
     }
-    printf("\t%1.9lf", B[i]);
     printf("\n");
-  }
-  printf("\n");
 }
 
 int main(int argc, char *argv[]) {
-  int n = 0, setInput = 0, i = 0, result = 0;
-  double *A, *B, *X, *tmp;
-  char *inputFile = "lss_71_01_in.txt";
-  char *outputFile = "lss_71_01_out.txt";
+    int n = 0, setInput = 0, i = 0, result = 0;
+    double *A, *B, *X, *tmp;
+    char *inputFile = "lss_71_01_in.txt";
+    char *outputFile = "lss_71_01_out.txt";
 
-  switch (validateParams(argc, argv)) {
-    case 1: {
-      if (flag_errors) {
-        printf("ValidationError: Wrong syntax of parameters. Too much filenames\n");
-      }
-      return 2;
-    }
-    case 2: {
-      if (flag_errors) {
-        printf(
-            "ValidationError: Wrong syntax of parameters. There is no such parameter or "
-            "you haven't set value to one of the parameters\n");
-      }
-      return 3;
-    }
-    default: {
-      break;
-    }
-  }
-
-  if (flag_help) {
-    printHelp();
-    return 0;
-  }
-
-  for (i = 1; i < argc; i++) {
-    if (argv[i][0] != '-') {
-      if (!setInput) {
-        if (i != 1) {
-          if (flag_errors) {
-            printf("ValidationError: Wrong order of parameters.\n");
-          }
-          return 4;
+    switch (validateParams(argc, argv)) {
+        case 1: {
+            if (flag_errors) {
+                printf("ValidationError: Wrong syntax of parameters. Too much filenames\n");
+            }
+            return 2;
         }
-        inputFile = argv[i];
+        case 2: {
+            if (flag_errors) {
+                printf(
+                    "ValidationError: Wrong syntax of parameters. There is no such parameter or "
+                    "you haven't set value to one of the parameters\n");
+            }
+            return 3;
+        }
+        default: {
+            break;
+        }
+    }
+
+    if (flag_help) {
+        printHelp();
+        return 0;
+    }
+
+    for (i = 1; i < argc; i++) {
+        if (argv[i][0] != '-') {
+            if (!setInput) {
+                if (i != 1) {
+                    if (flag_errors) {
+                        printf("ValidationError: Wrong order of parameters.\n");
+                    }
+                    return 4;
+                }
+                inputFile = argv[i];
+                if (!validateFile(inputFile)) {
+                    if (flag_errors) {
+                        printf("ValidationError: There is no such file.\n");
+                    }
+                    return 5;
+                }
+                setInput = 1;
+            } else {
+                if (i != 2) {
+                    if (flag_errors) {
+                        printf("ValidationError: Wrong order of parameters.\n");
+                    }
+                    return 4;
+                }
+                outputFile = argv[i];
+            }
+        }
+    }
+
+    if (!setInput) {
         if (!validateFile(inputFile)) {
-          if (flag_errors) {
-            printf("ValidationError: There is no such file.\n");
-          }
-          return 5;
+            if (flag_errors) {
+                printf("ValidationError: There is no such file.\n");
+            }
+            return 5;
         }
-        setInput = 1;
-      } else {
-        if (i != 2) {
-          if (flag_errors) {
-            printf("ValidationError: Wrong order of parameters.\n");
-          }
-          return 4;
+    }
+
+    switch (readInput(inputFile, &A, &B, &n)) {
+        case 1: {
+            if (flag_errors) {
+                printf("ValidationError. File is empty.\n");
+            }
+            return 6;
         }
-        outputFile = argv[i];
-      }
+        case 2: {
+            if (flag_errors) {
+                printf("ValidationError. n is not a positive integer.\n");
+            }
+            return 7;
+        }
+        case 3: {
+            if (flag_errors) {
+                printf("ValidationError. Not enough elements in the matrix.\n");
+            }
+            return 8;
+        }
+        case 4: {
+            if (flag_errors) {
+                printf("ValidationError. One of the elements of the matrix or constant terms vector is not a number.\n");
+            }
+            return 9;
+        }
+        default: {
+            break;
+        }
     }
-  }
 
-  if (!setInput) {
-    if (!validateFile(inputFile)) {
-      if (flag_errors) {
-        printf("ValidationError: There is no such file.\n");
-      }
-      return 5;
+    tmp = malloc(lss_memsize_71_01(n));
+    X = malloc(n * sizeof(double));
+
+    clock_t begin = clock();
+
+    if (flag_print_matrix) {
+        printSystem(n, A, B);
     }
-  }
 
-  switch (readInput(inputFile, &A, &B, &n)) {
-    case 1: {
-      if (flag_errors) {
-        printf("ValidationError. File is empty.\n");
-      }
-      return 6;
+    result = lss_71_01(n, A, B, X, tmp);
+    clock_t end = clock();
+
+    if (flag_print_matrix) {
+        printSystem(n, A, B);
     }
-    case 2: {
-      if (flag_errors) {
-        printf("ValidationError. n is not a positive integer.\n");
-      }
-      return 7;
+
+    double timeSpent = (double)(end - begin) / CLOCKS_PER_SEC;
+
+    if (flag_time) {
+        printf("Execution time: %1.9lf\n", timeSpent);
     }
-    case 3: {
-      if (flag_errors) {
-        printf("ValidationError. Not enough elements in the matrix.\n");
-      }
-      return 8;
-    }
-    case 4: {
-      if (flag_errors) {
-        printf("ValidationError. One of the elements of the matrix or constant terms vector is not a number.\n");
-      }
-      return 9;
-    }
-    default: {
-      break;
-    }
-  }
 
-  tmp = malloc(lss_memsize_71_01(n));
-  X = malloc(n * sizeof(double));
+    writeAnswer(outputFile, n, X, result);
 
-  clock_t begin = clock();
+    free(tmp);
+    free(X);
 
-  if (flag_print_matrix) {
-    printSystem(n, A, B);
-  }
-
-  result = lss_71_01(n, A, B, X, tmp);
-  clock_t end = clock();
-
-  if (flag_print_matrix) {
-    printSystem(n, A, B);
-  }
-
-  double timeSpent = (double)(end - begin) / CLOCKS_PER_SEC;
-
-  if (flag_time) {
-    printf("Execution time: %1.9lf\n", timeSpent);
-  }
-
-  writeAnswer(outputFile, n, X, result);
-
-  free(tmp);
-  free(X);
-
-  return result;
+    return result;
 }
